d9129b99f80cf40ec66226f7af0f7db264816174
José María Berná Chamber <jbernach@gmail.com>
2019-07-03 13:27:39 +0200

    first playable version

--- script.js
+++ script.js
@@ -9,13 +9,26 @@ function createBoxes() {
         const newBox = createBox();
         container.appendChild(newBox);
     }
+
+    document.addEventListener('keydown', event => {
+        if (event.code == 'Space') {
+            event.preventDefault();
+            console.log("Space pressed");
+
+            const boxToOpen = document.querySelector('.box[data-opened=false]');
+            if (boxToOpen) {
+                var clickEvent = new Event('click');
+                boxToOpen.dispatchEvent(clickEvent);
+            }            
+        }
+    });
 }
 
 function createBox() {    
     const newBox = document.createElement('div');
     newBox.className = 'box';
     newBox.setAttribute('data-opened', 'false');
-    newBox.setAttribute('data-content', 'nothing');
+    newBox.setAttribute('data-content', assignContent());
 
     newBox.addEventListener('click', event => {
         if (newBox.getAttribute('data-opened') === 'true') {
@@ -26,4 +39,16 @@ function createBox() {
     });
 
     return newBox;
+}
+
+function assignContent() {
+    const legendary = 0.002;
+    const r = Math.random();
+
+    if (r <= legendary) {
+        return 'legendary';
+    }
+
+    
+    return 'nothing';
 }
