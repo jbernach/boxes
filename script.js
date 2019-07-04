ecdd76f4eaab3d03090e6332c2111243cbb9f635
José María Berná Chamber <jbernach@gmail.com>
2019-07-04 08:44:22 +0200

    boxes have brawler names

--- script.js
+++ script.js
@@ -1,4 +1,108 @@
-var numBoxes = 2000;
+const numBoxes = 2000;
+
+const availableBrawlers = [
+    {
+        name: 'EL PRIMO',
+        quality: 'special',
+        resource: 'img/elprimo.png'
+    },
+    {
+        name: 'BARLEY',
+        quality: 'special',
+        resource: 'img/barley.png'
+    },
+    {
+        name: 'POCO',
+        quality: 'special',
+        resource: 'img/poco.png'
+    },
+    {
+        name: 'ROSA',
+        quality: 'special',
+        resource: 'img/rosa.png'
+    },
+    {
+        name: 'RICO',
+        quality: 'superspecial',
+        resource: 'img/rico.png'
+    },
+    {
+        name: 'DARRYL',
+        quality: 'superspecial',
+        resource: 'img/darryl.png'
+    },
+    {
+        name: 'PENNY',
+        quality: 'superspecial',
+        resource: 'img/penny.png'
+    },
+    {
+        name: 'CARL',
+        quality: 'superspecial',
+        resource: 'img/carl.png'
+    },
+    {
+        name: 'PIPER',
+        quality: 'epic',
+        resource: 'img/piper.png'
+    },
+    {
+        name: 'PAM',
+        quality: 'epic',
+        resource: 'img/pam.png'
+    },
+    {
+        name: 'FRANK',
+        quality: 'epic',
+        resource: 'img/frank.png'
+    },
+    {
+        name: 'BIBI',
+        quality: 'epic',
+        resource: 'img/bibi.png'
+    },
+    {
+        name: 'MORTIS',
+        quality: 'mythical',
+        resource: 'img/mortis.png'
+    },
+    {
+        name: 'GENIO',
+        quality: 'mythical',
+        resource: 'img/genio.png'
+    },
+    {
+        name: 'TARA',
+        quality: 'mythical',
+        resource: 'img/tara.png'
+    },
+    {
+        name: 'SPIKE',
+        quality: 'legendary',
+        resource: 'img/spike.png'
+    },
+    {
+        name: 'CROW',
+        quality: 'legendary',
+        resource: 'img/crow.png'
+    },
+    {
+        name: 'LEON',
+        quality: 'legendary',
+        resource: 'img/leon.png'
+    }    
+];
+
+const probabilities = {
+    'special': 2.7168/100.0,
+    'superspecial': 1.2069/100.0,
+    'epic': 0.5472/100.0,
+    'mythical': 0.2496/100.0,
+    'legendary': 0.0768/100.0
+};
+
+var achieved = [];
+var placedInBox = [];
 
 function createBoxes() {
     console.log('Creating ' + numBoxes + ' boxes...');
@@ -28,39 +132,63 @@ function createBox() {
     const newBox = document.createElement('div');
     newBox.className = 'box';
     newBox.setAttribute('data-opened', 'false');
-    newBox.setAttribute('data-content', assignContent());
+    var price = assignBrawler();
+    newBox.setAttribute('data-price', price);
 
+    if (price !== 'nothing') {
+        var b = getBrawlerByName(price);
+        newBox.setAttribute('data-quality', b.quality);
+    }
+    
     newBox.addEventListener('click', event => {
-        if (newBox.getAttribute('data-opened') === 'true') {
-            console.log("Box already opened.");
-        } else {
-            newBox.setAttribute('data-opened', 'true');
-        }
+       openBox(newBox);
     });
 
     return newBox;
 }
 
-function assignContent() {
-    const special = 2.7168/100.0;
-    const superspecial = 1.2069/100.0;
-    const epic = 0.5472/100.0;
-    const mythical = 0.2496/100.0;
-    const legendary = 0.0768/100.0;
-    
-    const r = Math.random();
+function assignBrawler() {
+    for (var i = 0; i < availableBrawlers.length; i++) {
+        var b = availableBrawlers[i];
 
-    if (r <= legendary) {
-        return 'legendary';
-    } else if (r <= mythical) {
-        return 'mythical';
-    } else if (r <= epic) {
-        return 'epic';
-    } else if (r <= superspecial) {
-        return 'superspecial';
-    } else if (r <= special) {
-        return 'special';
+        if (placedInBox.indexOf(b.name) === -1) {
+            var r = Math.random();
+            var prob = probabilities[b.quality];
+
+            if (r <= prob) {
+                placedInBox.push(b.name);
+                return b.name;
+            }
+        }
     }
-    
+
     return 'nothing';
+}
+
+function getBrawlerByName(name) {
+    for (var i = 0; i < availableBrawlers.length; i++) {
+        if (availableBrawlers[i].name === name) {
+            return availableBrawlers[i];
+        }
+    }
+
+    return {};
+}
+
+function openBox(box) {
+    if (box.getAttribute('data-opened') === 'true') {
+        console.log("Box already opened.");
+    } else {
+        box.setAttribute('data-opened', 'true');
+
+        var price = box.getAttribute('data-price');
+
+        if (price !== 'nothing') {
+            var b = getBrawlerByName(price);
+
+            const priceElement = document.createElement('div');
+            priceElement.innerText = b.name;
+            document.getElementById('prices').appendChild(priceElement);
+        }
+    }
 }
